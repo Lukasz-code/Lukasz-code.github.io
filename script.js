923c65401cee4e4b19efc0e6b2d28518c0804798
unknown <lukaszschweda@gmail.com>
2020-05-26 21:14:48 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://aqueous-beyond-29310.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://aqueous-beyond-29310.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
